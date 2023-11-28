ed58e2c2bba5a5eb8033ffe1acac5d3eca667cf8
hun2__2 <wognskec@hanyang.ac.kr>
2023-11-28 09:58:16 +0900

    [setting] 프로젝트 세팅 및 요구사항 정리
    1. eslint + prettier
    2. pinia로 전역상태관리
    3. vue-router로 라우터 연결

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from './MainPage.vue'
+import DetailPage from './DetailPage.vue'
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: MainPage
+    },
+    {
+      path: '/detail',
+      component: DetailPage
+    }
+  ]
+})
