462fae235db1d0477b0e1e67bece1a78281088bc
hun2__2 <wognskec@hanyang.ac.kr>
2023-11-28 11:26:49 +0900

    [setting] 스타일 세팅 + 경로별칭 추가
    1. tailwindCSS 세팅 및 잘 되는지 테스트
    2. 경로별칭으로 절대 경로 세팅

---
+++ src/components/TheIcon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ active?: boolean }>()
+</script>
+
+<template>
+  <div
+    :class="{ active }"
+    class="w-[30px] h-[30px] border-[1px] border-solid border-gray-400 rounded-full flex justify-center items-center cursor-pointer active:bg-blue-600 active:border-[1px] active:border-solid active:border-blue-600">
+    <span
+      :class="{ active }"
+      class="material-symbols-outlined opacity-0 rounded-full active:text-white active:opacity-100"
+      ><slot></slot>
+    </span>
+  </div>
+</template>
--- src/routes/MainPage.vue
+++ src/routes/MainPage.vue
@@ -1,3 +1,10 @@
 <template>
   <h1>Main Page!</h1>
+  <TheIcon active>search</TheIcon>
+  <TheIcon>add</TheIcon>
+  <TheIcon>check</TheIcon>
 </template>
+
+<script setup lang="ts">
+import TheIcon from '@/components/TheIcon.vue'
+</script>
