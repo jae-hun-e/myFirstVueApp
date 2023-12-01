6cf53a6a4d11b3818428989cc94dd36218271411
hun2__2 <wognskec@hanyang.ac.kr>
2023-12-01 10:46:03 +0900

    [fix] 404 page
    1. 404page 처리를 의해 vercel.json 추가
    -> vite와 안맞음
    2. vite와 호환을 위해 vite.config.ts에 server proxy추가
    
    + 로컬에서 한번에 실행하기 위해 concurrently를 통해 vite, vercel 동시 실행

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:2999',
+        changeOrigin: true
+      }
+    }
   }
 })
